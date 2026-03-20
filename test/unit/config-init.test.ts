55ae8a6e17a9fdc24406f69bc74b4028d24021e2
Crazyfire Lee <me@crazyfirelee.tw>
2026-03-20 05:44:04 +0000

    test: add unit tests for security-critical utilities
    Add tests for:
    - URL validation: verifies allow-list correctly accepts http/https/ssh
      and rejects javascript, file, data, ftp protocols
    - Atomic write: verifies write-then-rename pattern, no temp file leaks,
      and correct overwrites
    - Config init: verifies legacy config parsing, default fallbacks, user
      config merging, and undefined plugin filtering

---
+++ test/unit/config-init.test.ts
@@ -0,0 +1,103 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import test from 'ava';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const proxyquire = require('proxyquire').noCallThru();
+
+const notifyStub = () => {};
+const mapKeysStub = (keymaps: any) => keymaps;
+
+const {_init, _extractDefault} = proxyquire('../../app/config/init', {
+  '../notify': notifyStub,
+  '../utils/map-keys': mapKeysStub
+});
+
+test('_extractDefault parses valid config', (t) => {
+  const cfg = `module.exports = {
+    config: {
+      fontSize: 14
+    }
+  };`;
+
+  const result = _extractDefault(cfg);
+  t.is(result.config.fontSize, 14);
+});
+
+test('_extractDefault throws on missing module.exports', (t) => {
+  const cfg = 'var x = 1;';
+  t.throws(() => _extractDefault(cfg), {message: /module\.exports.*not set/});
+});
+
+test('_init provides defaults when config key is missing', (t) => {
+  const defaultCfg = {
+    config: {
+      fontSize: 12,
+      defaultProfile: 'default',
+      profiles: [{name: 'default', config: {}}]
+    },
+    keymaps: {},
+    plugins: [],
+    localPlugins: []
+  };
+
+  const result = _init({}, defaultCfg);
+  t.is(result.config.fontSize, 12);
+});
+
+test('_init merges user config over defaults', (t) => {
+  const defaultCfg = {
+    config: {
+      fontSize: 12,
+      fontFamily: 'Menlo',
+      defaultProfile: 'default',
+      profiles: [{name: 'default', config: {}}]
+    },
+    keymaps: {},
+    plugins: [],
+    localPlugins: []
+  };
+
+  const userCfg = {
+    config: {
+      fontSize: 16,
+      defaultProfile: 'default',
+      profiles: [{name: 'default', config: {}}]
+    },
+    keymaps: {},
+    plugins: ['hyper-snazzy'],
+    localPlugins: []
+  };
+
+  const result = _init(userCfg, defaultCfg);
+  t.is(result.config.fontSize, 16);
+  t.deepEqual(result.plugins, ['hyper-snazzy']);
+});
+
+test('_init filters out undefined plugins', (t) => {
+  const defaultCfg = {
+    config: {
+      fontSize: 12,
+      defaultProfile: 'default',
+      profiles: [{name: 'default', config: {}}]
+    },
+    keymaps: {},
+    plugins: [],
+    localPlugins: []
+  };
+
+  const userCfg = {
+    config: {
+      defaultProfile: 'default',
+      profiles: [{name: 'default', config: {}}]
+    },
+    keymaps: {},
+    plugins: ['valid-plugin', undefined, 'another-plugin'],
+    localPlugins: [undefined]
+  };
+
+  const result = _init(userCfg, defaultCfg);
+  t.deepEqual(result.plugins, ['valid-plugin', 'another-plugin']);
+  t.deepEqual(result.localPlugins, []);
+});
